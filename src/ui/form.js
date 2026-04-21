526c5085f0661bce27aa047ad5868207ca93d7e5
Kushal Zinzuvadia <kushal.zinzuvadia@simformsolutions.com>
2026-04-21 15:20:07 +0530

    feat: Add validations for date input, description length

--- src/ui/form.js
+++ src/ui/form.js
@@ -5,8 +5,23 @@ let editingId = null;
 const form = document.querySelector(".expense-form");
 
 export function initForm() {
+    const dateInput = document.querySelector("#date");
+
+    function getToday() {
+        return new Date().toISOString().split("T")[0];
+    }
+
+    const today = getToday();
+
+    if (!dateInput.value) {
+        dateInput.value = today;
+    }
+
+    dateInput.max = today;
+
     form.addEventListener("submit", (e) => {
         e.preventDefault();
+        const current = getToday();
 
         const expense = {
             id: Date.now(),
@@ -15,6 +30,23 @@ export function initForm() {
             date: document.getElementById("date").value,
             paidBy: document.getElementById("paidBy").value
         };
+
+        if (expense.date > current) {
+            alert("Future dates are not allowed.");
+            return;
+        }
+
+        const description = expense.description.trim();
+
+        if (description.length < 3) {
+            alert("Description must be at least 3 characters");
+            return;
+        }
+
+        if (description.length > 50) {
+            alert("Description exceed 50 characters");
+            return;
+        }
 
         if (editingId) {
             updateExpense(editingId, expense);
@@ -26,6 +58,7 @@ export function initForm() {
         renderExpenses();
         form.reset();
         form.querySelector('button[type="submit"]').textContent = "Add Expense";
+        dateInput.value = getToday();
     });
 }
 
