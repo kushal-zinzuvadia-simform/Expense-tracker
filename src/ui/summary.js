2327d52ac0ae34dc228b4efc82291d4484608f33
Kushal Zinzuvadia <kushal.zinzuvadia@simformsolutions.com>
2026-04-23 15:37:34 +0530

    feat: Balance statement logic for youOwe field

--- src/ui/summary.js
+++ src/ui/summary.js
@@ -1,8 +1,39 @@
 import { calculateBalances } from "../services/balanceService.js";
 import { getExpenses } from "../services/expenseService.js";
+import { getActiveUser, getUserNameById } from "../services/userService.js";
 
 export function renderSummary() {
     const expenses = getExpenses();
     const balances = calculateBalances(expenses);
-    console.log(balances);
+    const activeUserId = getActiveUser();
+    const activeUserName = getUserNameById(activeUserId);
+    const balanceList = document.querySelector(".balance-list");
+    const youOwe = document.querySelector(".you-owe");
+    const youAreOwed = document.querySelector(".you-owed");
+
+    balanceList.replaceChildren();
+
+    const owes = balances[activeUserId] || {};
+    let redTotal = 0, greenTotal = 0;
+
+    for (const [lender, amount] of Object.entries(owes)) {
+        const lenderId = Number(lender);
+        const statement = `${activeUserName} owes ${getUserNameById(lenderId)}`;
+
+        const item = document.createElement("div");
+        item.className = "balance-item";
+
+        const spanItem = document.createElement("span");
+        spanItem.textContent = statement;
+
+        const itemAmount = document.createElement("span");
+        itemAmount.className = "amount negative";
+        itemAmount.textContent = `₹${amount}`;
+        redTotal += amount;
+
+        item.append(spanItem, itemAmount);
+        balanceList.appendChild(item);
+    }
+
+    youOwe.textContent = `₹${redTotal}`;
 }
